eab1f63c23165852f0f1ba7def1e8e8d122cc71e
gratestas <stasgrate@gmail.com>
2023-10-15 16:39:31 +0300

    add missing change

--- src/pages/ActiveGame/styled.ts
+++ src/pages/ActiveGame/styled.ts
@@ -21,33 +21,3 @@ export const CardContainer = styled.div`
     flex-direction: column;
   }
 `;
-
-export const Card = styled.div`
-  position: relative;
-  flex: 1;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  padding: 20px;
-  margin: 0 10px;
-  background-color: #f9f9f9;
-  text-align: left;
-  @media (max-width: 768px) {
-    margin: 10px 0;
-  }
-`;
-
-export const Timer = styled.div`
-  position: absolute;
-  text-align: right;
-  top: 18px;
-  right: 16px;
-  width: 110px;
-  font-size: 14px;
-  font-weight: 500;
-`;
-
-export const PlayerInfo = styled.div`
-  font-size: 16px;
-  font-weight: 500;
-  margin-bottom: 10px;
-`;
